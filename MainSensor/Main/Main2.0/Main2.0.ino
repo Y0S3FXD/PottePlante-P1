17269ae2a83ede1de935a55cb1c0778a43df9c37
Marcus Schouw <22mschouw@unis.org>
2024-12-10 09:07:51 +0100

    small changes

--- MainSensor/Main/Main2.0/Main2.0.ino
+++ MainSensor/Main/Main2.0/Main2.0.ino
@@ -1,34 +1,27 @@
-// Include the DHT11 library for interfacing with the sensor.
 #include <DHT11.h>
 
-// Moisture setup
+// Globale variabler
 int moisturePin = A0;
 int maxValue = 1023;
 int minValue = 300;
 int pMoistureOld = 0;
 
-// Create an instance of the DHT11 class.
-// - For Arduino: Connect the sensor to Digital I/O Pin 7.
-DHT11 dht11(7);
+DHT11 dht11(7); //Digital I/O Pin 7
 
 void setup() {
-  // Humidity
   pinMode(moisturePin, INPUT);
 
   Serial.begin(9600);
-  
 }
 
 void loop() {
     int temperature = 0;
     int humidity = 0;
 
-    // Attempt to read the temperature and humidity values from the DHT11 sensor.
-
     int result = dht11.readTemperatureHumidity(temperature, humidity);
     int sensorValue = analogRead(moisturePin);
     int pMoisture = map(sensorValue, maxValue, minValue, 0, 100); // omregner til procent
-    
+
     if (abs(pMoistureOld - pMoisture) <= 10 && pMoisture <= 100 ) { // forskel på 10 eller mere i %
 
         if (result == 0) {
@@ -49,10 +42,5 @@ void loop() {
       // Serial.println("%, Stor forskel"); // skal fjernes før der skal laves csv fil 
       pMoistureOld = pMoisture;
     }
-
-    
-
-  
     delay(500);
 }
-
